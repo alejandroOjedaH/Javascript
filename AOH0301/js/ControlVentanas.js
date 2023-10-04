b710b7cb1d833dc14921a725a28d205e47fb7cac
alejandro <limonmagico1@gmail.com>
2023-10-04 23:50:45 +0200

    Ejercicio3

--- AOH0301/js/ControlVentanas.js
+++ AOH0301/js/ControlVentanas.js
@@ -1,5 +1,6 @@
-let largo =document.getElementById("windowHigh")
+let largo = document.getElementById("windowHigh")
 let ancho = document.getElementById("windowWidth");
+let move = document.getElementById("movement");
 let ventanaActual;
 let ventanas =[];
 
@@ -15,4 +16,25 @@ function cambiarAlturaAnchura(){
 
 function cerrarVentana(){
     ventanaActual.close();
+    ventanas.pop();
+    ventanaActual = ventanas[ventanas.length-1];
+}
+
+function arribaVentana(){
+    ventanaActual.moveBy(0, move.value*-1);
+    ventanaActual.focus();
+}
+function abajoVentana(){
+    ventanaActual.moveBy(0, move.value);
+    ventanaActual.focus();
+}
+
+function izquierdaVentana(){
+    ventanaActual.moveBy(move.value*-1,0);
+    ventanaActual.focus();
+}
+
+function derechaVentana(){
+    ventanaActual.moveBy(move.value,0);
+    ventanaActual.focus();
 }
